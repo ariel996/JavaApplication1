69d3fdf720ff26b19c9bb955a03909ae63f3afc7
ariel nana <arieldeguilique@hotmail.com>
2022-11-24 00:18:38 +0100

    touch properties

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -4,25 +4,53 @@
  */
 package javaapplication1;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
 
 /**
  *
  * @author ariel
  */
 public class JavaApplication1 extends Application {
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-        
-    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+    Button btn;
+     @Override
+    public void start(Stage primaryStage) {
+     // Create the button   
+         btn = new Button();
+         btn.setText("Click me please");
+         btn.setOnAction(e -> buttonClick());
+         
+         // Add the button to a layout pane
+         BorderPane pane = new BorderPane();
+         pane.setCenter(btn);
+         
+         // Add the layout pane to a scene
+         Scene scene = new Scene(pane, 300, 250);
+         
+         // Finalize and show the stage
+         primaryStage.setScene(scene);
+         primaryStage.setTitle("The Click Me App");
+         primaryStage.show();
+    }
+    
+    public void buttonClick()
+    {
+        if (btn.getText() == "Click me please!")
+        {
+            btn.setText("You clicked me!");
+        } else {
+            btn.setText("Click me please!");
+        }
     }
     
 }
