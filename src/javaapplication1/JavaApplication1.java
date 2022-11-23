7cf12615fcc64ffca4a49ffcd1ecab6539715911
ariel nana <arieldeguilique@hotmail.com>
2022-11-23 23:41:12 +0100

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ariel
+ */
+public class JavaApplication1 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
